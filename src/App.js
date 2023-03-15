9ac1f75706b2b8590a24387b357d322661e28742
wangpeng <wangpeng022@sina.cn>
2023-03-15 18:47:12 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import board from './images/board.png';
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={board} className="App-logo" alt="logo" />
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
